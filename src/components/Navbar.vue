4b7ff36f8dc961537f513a120663b5bae83add65
nasser <anasserabdrabo@gmail.com>
2021-05-29 01:10:28 +0200

    feat: :art: Basic structure for navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="shadow-sm">
+    <div
+      class="container d-flex align-items-center justify-content-between py-3"
+    >
+      <div class="logo">LOGO</div>
+      <nav>
+        <a href="#" class="navItem">Home</a>
+        <a href="#" class="navItem">Workout Plans</a>
+        <a href="#" class="navItem">Meals</a>
+        <a href="#" class="navItem">Exercises</a>
+        <a href="#" class="navItem btn btn-primary">Login</a>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.navItem {
+  display: inline-block;
+  margin-left: 16px;
+  text-decoration: none;
+}
+</style>
