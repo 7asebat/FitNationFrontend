e837e4fd20402b2958558b8cbaf7b0f21600a911
nasser <anasserabdrabo@gmail.com>
2021-05-29 04:50:48 +0200

    chore: :art: Add fontAwesome icons

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from "./router";
 import store from "./store";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
+import "@/plugins/fontAwesome";
+
 import "./assets/styles/global.scss";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
