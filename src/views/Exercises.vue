b6325f76cb0f3889662063ac69d8b47d07b892a3
nasser <anasserabdrabo@gmail.com>
2021-06-06 03:44:45 +0200

    feat: :fire: Add dynamic muscle diagram

--- src/views/Exercises.vue
+++ src/views/Exercises.vue
@@ -1,52 +1,36 @@
 <template>
   <div class="container">
-    <div
-      id="exercises-view"
-      class="
-        d-flex
-        flex-column
-        justify-content-center
-        align-items-center
-        text-center
-        py-4
-        px-4
-      "
-    >
-      <div class="d-flex flex-column align-items-center">
-        <div
-          v-if="$route.params.id === undefined"
-          class="d-flex flex-column align-items-center mb-5"
-        >
-          <h1 class="c-image-text-title text-dark my-5">Exercises</h1>
-          <div class="text-secondary w-75">
-            {{ subtitle }}
-          </div>
-
-          <div id="muscle-text-action" class="d-flex flex-row mt-5">
-            <!-- TODO(Abdelrahman) Make this button wider -->
-            <div>
-              <a href="" class="btn btn-primary text-uppercase"
-                >Browse all exercises</a
-              >
+    <div id="exercises-view" class="pt-4">
+      <div :class="{ 'c-exerccises__header--small': !selectedMuscle }">
+        <div v-show="!selectedMuscle" class="mb-4">
+          <div class="d-flex flex-column align-items-center text-center">
+            <h1 class="c-image-text-title text-dark">Exercises</h1>
+            <div class="text-secondary">
+              {{ subtitle }}
             </div>
 
-            <div class="d-flex align-items-center ml-5">
-              <a href="" class="c-no-underline text-secondary"
-                >Need help? Get a personal trainer</a
-              >
+            <div id="muscle-text-action" class="d-flex flex-row mt-3">
+              <!-- TODO(Abdelrahman) Make this button wider -->
+              <div>
+                <a href="" class="btn btn-primary text-uppercase"
+                  >Browse all exercises</a
+                >
+              </div>
+
+              <div class="d-flex align-items-center ml-5">
+                <a href="" class="c-no-underline text-secondary"
+                  >Need help? Get a personal trainer</a
+                >
+              </div>
             </div>
           </div>
         </div>
 
-        <MuscleSelection
-          v-else
-          class="text-dark my-5"
-          :selectedMuscle="muscles[selectedMuscle]"
-        />
+        <MuscleSelector @selectMuscle="selectMuscle" />
+      </div>
 
-        <div class="mt-5 w-100">
-          <PopularExercises />
-        </div>
+      <div class="mt-5 w-100">
+        <PopularExercises />
       </div>
     </div>
   </div>
@@ -57,7 +41,8 @@ export default {
   name: "Exercises",
 
   components: {
-    MuscleSelection: () => import("@/components/MuscleSelection.vue"),
+    // MuscleSelection: () => import("@/components/MuscleSelection.vue"),
+    MuscleSelector: () => import("@/components/Exercises/MuscleSelector.vue"),
     PopularExercises: () => import("@/components/PopularExercises.vue"),
   },
 
@@ -67,10 +52,22 @@ export default {
     subtitle:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   }),
+
+  methods: {
+    selectMuscle(selectedMuscle) {
+      this.selectedMuscle = selectedMuscle;
+    },
+  },
 };
 </script>
 
 <style lang="scss">
+.c-exerccises__header--small {
+  max-width: 700px;
+  margin-right: auto;
+  margin-left: auto;
+}
+
 .c-image-text-title {
   font-size: 62px !important;
   font-family: "Bebas Neue", cursive;
