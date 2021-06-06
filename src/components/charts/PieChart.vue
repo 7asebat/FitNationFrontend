72021d3ece15a782cf6ed3701d487a1f48dd5945
nasser <anasserabdrabo@gmail.com>
2021-06-07 00:40:14 +0200

    Add aggregations in panel dashboard

---
+++ src/components/charts/PieChart.vue
@@ -0,0 +1,43 @@
+<script>
+import { Pie } from "vue-chartjs";
+export default {
+  extends: Pie,
+  data() {
+    return {
+      chartOptions: {
+        hoverBorderWidth: 20,
+      },
+    };
+  },
+  props: ["labels", "data"],
+  computed: {
+    chartData() {
+      const colors = this.data.map(
+        () =>
+          "#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6)
+      );
+      return {
+        hoverBackgroundColor: "red",
+        hoverBorderWidth: 10,
+        labels: this.labels,
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: colors,
+            data: this.data,
+          },
+        ],
+      };
+    },
+  },
+  mounted() {
+    // this.chartData is created in the mixin.
+    // If you want to pass options please create a local options object
+    this.renderChart(this.chartData, this.chartOptions);
+
+    // const canvas = document.getElementById("pie-chart");
+    // canvas.height = 500;
+    // canvas.width = 500;
+  },
+};
+</script>
--- src/views/Panel/Dashboard.vue
+++ src/views/Panel/Dashboard.vue
@@ -1,5 +1,111 @@
 <template>
   <div>
     <h1>Panel Dashboard</h1>
+    <p class="mb-3 text-secondary">
+      A quick overview about how the platform behaves
+    </p>
+    <div class="row">
+      <div class="col-12 col-md-6">
+        <div class="row">
+          <div class="col-12 col-md-6 mb-3">
+            <div class="statistics_card p-3 bg-primary text-light">
+              <h1 class="m-0">
+                <i class="fas fa-users"></i>
+                {{ aggregations.clients }}
+              </h1>
+              <h5>Clients</h5>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-6 mb-3">
+            <div class="statistics_card p-3 bg-warning text-light">
+              <h1 class="m-0">
+                <i class="fas fa-dumbbell"></i>
+                {{ aggregations.exercises }}
+              </h1>
+              <h5>Exercises</h5>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-6 mb-3">
+            <div class="statistics_card p-3 bg-success text-light">
+              <h1 class="m-0">
+                <i class="fas fa-utensils"></i>
+                {{ aggregations.food }}
+              </h1>
+              <h5>Food</h5>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-6 mb-3">
+            <div class="statistics_card p-3 bg-info text-light">
+              <h1 class="m-0">
+                <i class="fas fa-running"></i>
+                {{ aggregations.trainers }}
+              </h1>
+              <h5>Trainers</h5>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-6 mb-3">
+            <div class="statistics_card p-3 bg-dark text-light">
+              <h1 class="m-0">
+                <i class="fas fa-dumbbell"></i>
+                {{ aggregations.workout_plans }}
+              </h1>
+              <h5>Workout Plans</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-6">
+        <PieChart :labels="['biceps', 'abs']" :data="[12, 234]" />
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  created() {
+    this.getDashboardData();
+  },
+
+  data() {
+    return {
+      aggregations: {},
+    };
+  },
+
+  computed: {
+    pieChartLabels() {
+      // const muscles = this.$store.state.enums.muscles;
+      return true;
+    },
+  },
+
+  methods: {
+    async getDashboardData() {
+      try {
+        const response = await this.axios.get("admins/dashboard");
+        const aggregations = response.data.counts;
+
+        this.aggregations = aggregations;
+      } catch (err) {
+        this.$errorsHandler(err);
+      }
+    },
+  },
+
+  components: {
+    PieChart: () => import("@/components/charts/PieChart"),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.statistics_card {
+  border-radius: $border-radius;
+}
+</style>
